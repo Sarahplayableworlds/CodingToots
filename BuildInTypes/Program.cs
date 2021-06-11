7b3c6e4bc40a9d0d2d924de81496d3d2ecbeaff9
Sarahplayableworlds <83302317+Sarahplayableworlds@users.noreply.github.com>
2021-06-10 18:45:17 -0700

    We all float here
    Except it isn't showing in the console =/

--- BuildInTypes/Program.cs
+++ BuildInTypes/Program.cs
@@ -13,11 +13,21 @@ namespace BuildInTypes
             var x = 234;
             Console.WriteLine(x.GetType());
 
-            Console.WriteLine($"int: minumum {int.MinValue}, maximum {int.MaxValue}");
-            Console.WriteLine($"long: minumum {long.MinValue}, maximum {long.MaxValue}");
+            Console.WriteLine($"int: minimum {int.MinValue}, maximum {int.MaxValue}");
+            Console.WriteLine($"long: minimum {long.MinValue}, maximum {long.MaxValue}");
+
+            Console.WriteLine();
 
             Console.WriteLine($"float: minimum {float.MinValue}, maximum {float.MaxValue}");
             Console.WriteLine($"double: minimum {double.MinValue}, maximum {double.MaxValue}");
+
+            float f = 123.456789012345678f;
+            double d = 123.456789012345678;
+            Console.WriteLine($"f is {f:F99}");
+            Console.WriteLine($"d is {d:F99}");
+
+            var y = 123.4;
+            Console.WriteLine(y.GetType());
         }
     }
 }

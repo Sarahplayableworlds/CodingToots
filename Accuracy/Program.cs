f8bf269126b60f690c5b9e20e0dbd0b93752eddf
Sarahplayableworlds <83302317+Sarahplayableworlds@users.noreply.github.com>
2021-06-22 16:59:08 -0700

    Math Difference

---
+++ Accuracy/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accuracy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            double result1 = 0.1 * 8;
+            double Result2 = 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1;
+
+            Console.WriteLine($"1st result is {result1}");
+            Console.WriteLine($"2nd result is {Result2}");
+
+            double difference = result1 - Result2;
+            Console.WriteLine($"The difference is {difference}");
+        }
+    }
+}

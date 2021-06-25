e85701a3789063c2084ceef07e997312c261e121
Sarahplayableworlds <83302317+Sarahplayableworlds@users.noreply.github.com>
2021-06-24 17:28:44 -0700

    Spicy Beef Flavor

--- BoolExp/Program.cs
+++ BoolExp/Program.cs
@@ -12,17 +12,34 @@ namespace BoolExp
             decimal applePrice = 12.60m;
             decimal orangePrice = 4.50m;
 
-            Console.WriteLine(apples == oranges);
-            Console.WriteLine(apples != oranges);
-            Console.WriteLine($"{apples} > {oranges} is {apples > oranges}");
-            Console.WriteLine($"{apples} < {oranges} is {apples > oranges}");
-            Console.WriteLine($"{apples} >= {oranges} is {apples > oranges}");
-            Console.WriteLine($"{apples} <= {oranges} is {apples > oranges}");
-            Console.WriteLine($"{apples} is int is {apples is int}");
+            bool moreApples;
+            bool applesAreDearer;
 
-            Console.WriteLine($"Reducing apple cost: {(apples > oranges) && (applePrice > orangePrice)}");
+            moreApples = (apples > oranges);
+            applesAreDearer = (applePrice > orangePrice);
+            Console.WriteLine($"We have more apples: {moreApples}");
+            Console.WriteLine($"Apples are dearer: {applesAreDearer}");
 
-            Console.WriteLine($"Reducing apple cost: {(apples > oranges) || (applePrice > orangePrice)}");
+            //Console.WriteLine($"Reduce apple cost: {moreApples && applesAreDearer}");
+            //Console.WriteLine($"Reducing apples cost: {moreApples || applesAreDearer}");
+
+            bool moreApplesAndDearer = moreApples && applesAreDearer;
+            bool moreApplesOrDearer = moreApples || applesAreDearer;
+            Console.WriteLine($"Reducing apple cost: {moreApplesAndDearer}");
+            Console.WriteLine($"Reducing apple cost: {moreApplesOrDearer}");
+
+            //Console.WriteLine(apples == oranges);
+            //Console.WriteLine(apples != oranges);
+            //Console.WriteLine($"{apples} > {oranges} is {apples > oranges}");
+            //Console.WriteLine($"{apples} < {oranges} is {apples > oranges}");
+            //Console.WriteLine($"{apples} >= {oranges} is {apples > oranges}");
+            //Console.WriteLine($"{apples} <= {oranges} is {apples > oranges}");
+            //Console.WriteLine($"{apples} is int is {apples is int}");
+
+            //Console.WriteLine($"Reducing apple cost: {(apples > oranges) && (applePrice > orangePrice)}");
+
+            //Console.WriteLine($"Reducing apple cost: {(apples > oranges) || (applePrice > orangePrice)}");
+
         }
     }
 }

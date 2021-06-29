0dbdf2152712990771be394df7a3daa65b95dfb5
Sarahplayableworlds <83302317+Sarahplayableworlds@users.noreply.github.com>
2021-06-29 16:09:01 -0700

    Driving myCar

---
+++ ClassesIntro/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ClassesIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Car myCar = new Car();
+            Car anotherCar = new Car();
+
+            myCar.Accelerate();
+            anotherCar.Brake();
+        }
+    }
+
+    class Car
+    {
+        public void Accelerate()
+        {
+            Console.WriteLine("You're going faster.");
+        }
+
+        public void Brake()
+        {
+            Console.WriteLine("You're going slower.");
+        }
+    }
+}

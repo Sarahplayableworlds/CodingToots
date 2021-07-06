12c0f448d5b770ffae82559e91197d8547a0afc2
Sarahplayableworlds <83302317+Sarahplayableworlds@users.noreply.github.com>
2021-07-06 13:03:53 -0700

    WhileLoops

--- SimpleMenu/Program.cs
+++ SimpleMenu/Program.cs
@@ -6,24 +6,23 @@ namespace SimpleMenu
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please choose one of the following options");
-            Console.WriteLine("1 - Cappucino");
-            Console.WriteLine("2 - Latte");
-            Console.WriteLine("3 - Americano");
-            Console.WriteLine("4 - Mocha");
-            Console.WriteLine("5 - Macchiatio");
-            Console.WriteLine("6 - Espresso");
-            Console.WriteLine("Q - Quit");
+            char userChoice = ' ';
+            //while (!userChoice.Equals('q'))
+            while (!(userChoice.Equals('q') || userChoice.Equals('Q')))
+            {
+                Console.WriteLine("Please choose one of the following options");
+                Console.WriteLine("1 - Cappucino");
+                Console.WriteLine("2 - Latte");
+                Console.WriteLine("3 - Americano");
+                Console.WriteLine("4 - Mocha");
+                Console.WriteLine("5 - Macchiatio");
+                Console.WriteLine("6 - Espresso");
+                Console.WriteLine("Q - Quit");
 
-            ConsoleKeyInfo userChoice = Console.ReadKey(true);
-            Console.WriteLine("You Chose " + userChoice);
-
-            var x = userChoice.Key;
-            char y = userChoice.KeyChar;
-            var z = userChoice.Modifiers;
-            Console.WriteLine("Key: " + x);
-            Console.WriteLine("KeyChar: " + y);
-            Console.WriteLine("Modifiers: " + z);
+                userChoice = Console.ReadKey(true).KeyChar;
+                Console.WriteLine("You Chose " + userChoice);
+            }
+         
         }
     }
 }

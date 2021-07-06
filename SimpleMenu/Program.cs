fa0eef2c189d1aa03cde451921a2453b48390f91
Sarahplayableworlds <83302317+Sarahplayableworlds@users.noreply.github.com>
2021-07-06 13:26:35 -0700

    ChallengeSolutions

--- SimpleMenu/Program.cs
+++ SimpleMenu/Program.cs
@@ -7,22 +7,23 @@ namespace SimpleMenu
         static void Main(string[] args)
         {
             char userChoice = ' ';
-            //while (!userChoice.Equals('q'))
-            while (!(userChoice.Equals('q') || userChoice.Equals('Q')))
-            {
-                Console.WriteLine("Please choose one of the following options");
-                Console.WriteLine("1 - Cappucino");
-                Console.WriteLine("2 - Latte");
-                Console.WriteLine("3 - Americano");
-                Console.WriteLine("4 - Mocha");
-                Console.WriteLine("5 - Macchiatio");
-                Console.WriteLine("6 - Espresso");
-                Console.WriteLine("Q - Quit");
 
-                userChoice = Console.ReadKey(true).KeyChar;
+            Console.WriteLine("Please choose one of the following options");
+            Console.WriteLine("1 - Cappucino");
+            Console.WriteLine("2 - Latte");
+            Console.WriteLine("3 - Americano");
+            Console.WriteLine("4 - Mocha");
+            Console.WriteLine("5 - Macchiatio");
+            Console.WriteLine("6 - Espresso");
+            Console.WriteLine("Q - Quit");
+
+            while (!userChoice.Equals('q'))
+            //while (!(userChoice.Equals('q') || userChoice.Equals('Q')))
+            {
+                userChoice = char.ToLower(Console.ReadKey(true).KeyChar);
                 Console.WriteLine("You Chose " + userChoice);
             }
-         
+
         }
     }
 }

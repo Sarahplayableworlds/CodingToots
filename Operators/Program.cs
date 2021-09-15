2b7e0e90bbefeb3e6e63f4540740766c4cab5a9d
Sarahplayableworlds <83302317+Sarahplayableworlds@users.noreply.github.com>
2021-09-15 10:03:38 -0700

    Hello Operator

---
+++ Operators/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer = 7 + 3 * 4;
+            Console.WriteLine(answer);
+        }
+    }
+}

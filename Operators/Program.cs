bcc6b05597747cd8623583e11c54d45ba60e5e5a
Sarahplayableworlds <83302317+Sarahplayableworlds@users.noreply.github.com>
2021-09-21 13:48:19 -0700

    The bad

--- Operators/Program.cs
+++ Operators/Program.cs
@@ -6,8 +6,19 @@ namespace Operators
     {
         static void Main(string[] args)
         {
-            int answer = 7 + 3 * 4;
-            Console.WriteLine(answer);
+            //int answer = 7 + 3 * 4;
+            //Console.WriteLine(answer);
+
+            //double radius = 12;
+            //double area = Math.PI * radius * radius;
+
+            int x = 3;
+            int y = x++;
+            Console.WriteLine($"Using x++, x is {x} amd y is {y}");
+
+            x = 3;
+            y = ++x;
+            Console.WriteLine($"Using x++, x is {x} amd y is {y}");
         }
     }
 }

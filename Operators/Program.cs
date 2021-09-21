ad5f05de56bdcc8bb38fac18a9f9edc16e63b775
Sarahplayableworlds <83302317+Sarahplayableworlds@users.noreply.github.com>
2021-09-21 13:59:52 -0700

    Maths

--- Operators/Program.cs
+++ Operators/Program.cs
@@ -6,19 +6,22 @@ namespace Operators
     {
         static void Main(string[] args)
         {
-            //int answer = 7 + 3 * 4;
-            //Console.WriteLine(answer);
+            double a = 12 * 3 / 4;
+            double b = (12 * 3) / 4;
+            double c = 12 * (3 / 4);
+            Console.WriteLine($"{a}, {b}, {c},");
+            Console.WriteLine();
 
-            //double radius = 12;
-            //double area = Math.PI * radius * radius;
+            double d = 12.0 / 3.0 / 4.0;
+            double e = (12.0 / 3.0) / 4.0;
+            double f = 12.0 / (3.0 / 4.0);
+            Console.WriteLine($"{d}, {e}, {f}");
+            Console.WriteLine();
 
-            int x = 3;
-            int y = x++;
-            Console.WriteLine($"Using x++, x is {x} amd y is {y}");
-
-            x = 3;
-            y = ++x;
-            Console.WriteLine($"Using x++, x is {x} amd y is {y}");
+            int x = 12 + 3 - 4;
+            int y = (12 + 3) - 4;
+            int z = 12 + (3 - 4);
+            Console.WriteLine($"{x}, {y}, {z}");
         }
     }
 }
